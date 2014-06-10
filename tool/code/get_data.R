b2a7399a8b6f2419f73be25eb8a7dd2fe213ce8c
Luis Capelo <luiscape@gmail.com>
2014-06-10 16:43:39 -0400

    making the code more general

--- tool/code/get_data.R
+++ tool/code/get_data.R
@@ -6,10 +6,21 @@ library(rjson)
 library(RCurl)
 
 # Load data using the ScraperWiki SQL API.
-url <- getURL('https://ds-ec2.scraperwiki.com/zaflugd/iokwwtf3ldspuao/sql?q=select%20*%20from%20hdx_repo_analytics')
-getData <- function() { 
-    b <- fromJSON(url)
-    for (i in 1:length(b)) { 
+base_url <- 'https://ds-ec2.scraperwiki.com/zaflugd/iokwwtf3ldspuao/sql'
+
+# The sql query should be made depending on the tables
+# that you would like to visualize.
+table_name <- 'hdx_repo_analytics'  # table name with the viz data.
+sql_query <- paste('?q=select%20*%20from%20', table_name, sep = "")
+
+# Getting a query url.
+query_url <- getURL(paste(base_url, sql_query, sep = ""))
+
+# getData function that fetches the JSON and
+# parses is into a data.frame.
+getData <- function() {
+    b <- fromJSON(query_url)
+    for (i in 1:length(b)) {
         c <- b[[i]]
         if (i == 1) d <- c
         else d <- cbind(d, c)
@@ -19,11 +30,14 @@ getData <- function() {
 }
 data <- getData()
 
+### Visualization Specifig Handling ###
+# The code below is indended to shape the data
+# to match the needs of the specific visualization intended.
 
 # Leave only the columns of interest
-# and store the resulting objects into 
+# and store the resulting objects into
 # different CSV files.
-number_datasets <- data.frame(as.numeric(data$Number_of_Datasets), 
+number_datasets <- data.frame(as.numeric(data$Number_of_Datasets),
                               as.character(data$Date_and_Time))
 
 other_data <- data.frame(as.numeric(data$Number_of_Organizations),
@@ -35,5 +49,7 @@ names(number_datasets) <- c('Number_of_Datasets', 'Date_and_Time')
 names(other_data) <- c('Number_of_Organizations', 'Number_of_Users', 'Date_and_Time')
 
 # Storing data.
-write.csv(number_datasets, 'http/data/number_of_datasets.csv', row.names = FALSE)
-write.csv(other_data, 'http/data/other_data.csv', row.names = FALSE)
+write.csv(number_datasets, '~/tool/http/data/number_of_datasets.csv', row.names = FALSE)
+write.csv(other_data, '~/tool/http/data/other_data.csv', row.names = FALSE)
+
+cat('Data for chart has been refreshed.')
